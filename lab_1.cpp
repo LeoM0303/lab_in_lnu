b82daacd304a2efbd601e3e2b5fe1e0df5a4de58
Lev Muliarchuk <139563996+LeoM0303@users.noreply.github.com>
2024-09-03 13:11:20 +0300

    lab_1.cpp

---
+++ lab_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <locale>
+
+int main(){
+    /*setlocale(LC_ALL, "ua_UK");
+    std::cout << "Привіт світ!" << "\n";*/
+
+    std::cout << "This is my first program!" << "\n";
+
+    return 0;
+}
