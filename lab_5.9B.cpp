9c5c87c7fcfa9f083f106da0e7c8c840bcfae4da
Lev Muliarchuk <139563996+LeoM0303@users.noreply.github.com>
2024-10-09 23:20:19 +0300

    Update lab_5.9B.cpp

--- lab_5.9B.cpp
+++ lab_5.9B.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <iomanip>
 #include <locale>
-
+ 
 int main() {
-    setlocale(LC_ALL, "ua_UK");
-    std::cout << std::fixed << std::setprecision(6);  // Встановлюємо точність виводу до 6 десяткових знаків
-    std::cout << "См³\t\tМ³\t\tЛітри\n";
-
+    setlocale(LC_ALL, "uk_UA");
+    // Заголовок таблиці
+    std::cout << std::setw(10) << "см^3" << std::setw(15) << "м^3" << std::setw(15) << "літри" << std::endl;
     for (int cm3 = 1000; cm3 <= 2000; cm3 += 50) {
-        double m3 = cm3 * 1e-6;     // Перетворення в кубічні метри
-        double liters = cm3 * 0.001;  // Перетворення в літри
-        std::cout << cm3 << "\t\t" << m3 << "\t\t" << liters << "\n";  // Виведення результатів
+        // Перетворення
+        double m3 = cm3 * 0.000001;  // см³ -> м³
+        double liters = cm3 * 0.001; // см³ -> літри
+        // Виведення значень у вигляді таблиці
+        std::cout << std::setw(10) << cm3 << std::setw(15) << std::fixed << std::setprecision(6) << m3 
+<< std::setw(15) << liters << std::endl;
     }
-
+ 
     return 0;
 }
