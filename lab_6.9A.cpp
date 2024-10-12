a03a1dfe4f2b217dc77e706400d687f438435308
Lev Muliarchuk <139563996+LeoM0303@users.noreply.github.com>
2024-10-12 14:23:13 +0300

    Update lab_6.9A.cpp

--- lab_6.9A.cpp
+++ lab_6.9A.cpp
@@ -3,26 +3,24 @@
 using namespace std;
  
 int main() {
-    double x, epsilon, sum = 0.0, term;
-    int n = 1;  
-    
-    // Input x (in radians) and epsilon
+    double x, sum = 0.0, term;
+    int n, i = 1;  
+    // Input x (in radians) and number of terms n
     cout << "Enter the value of x (in radians): ";
     cin >> x;
-    cout << "Enter the precision epsilon: ";
-    cin >> epsilon;
+    cout << "Enter the number of terms n: ";
+    cin >> n;
  
     // First term (sin(x)) calculation
     term = sin(x);
     sum += term;
  
-    // Continue adding terms until the term is smaller than epsilon
-    while (fabs(term) > epsilon) {
-        n++;  // Increment the power of sin(x)
-        term = pow(sin(x), n);  // Calculate sin^n(x)
+    // Calculate the sum for the next (n - 1) terms
+    while (i < n) {
+        i++; 
+        term = pow(sin(x), i);  
         sum += term;
     }
- 
  
     cout << "The sum of the series is: " << sum << endl;
     return 0;
